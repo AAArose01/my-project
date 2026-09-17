10f93422f5d91a5cb6853048fc2ac50755340821
AAArose01 <19730032953@163.com>
2026-09-17 23:26:32 +0800

    新增乘法函数

--- hello.c
+++ hello.c
@@ -4,10 +4,14 @@ int add(int a, int b) {
 	return a+b;
 }
 
+int mul(int a,int b) {
+	return a*b;
+}
 int main(void) {
 	printf("Hello, Github！I am learning Git.\n");
 	printf("This is edited locally.\n");
 	printf("This is edited on Github.\n");
 	printf("1 + 2 = %d\n",  add(1, 2));
+	printf("2 * 3 =%d\n", mul(2,3));
      return 0:
 }
