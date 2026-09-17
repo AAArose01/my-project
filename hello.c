ade9c82766755555eddc2a625175fcc1c009529b
AAArose01 <19730032953@163.com>
2026-09-17 22:52:49 +0800

    新增加法函数

--- hello.c
+++ hello.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+int add(int a, int b) {
+	return a+b;
+}
+
 int main(void) {
-     printf("Hello, Github！I am learning Git.\n");
-     printf("This is my second commit.\n");
+	printf("Hello, Github！I am learning Git.\n");
+	printf("This is my second commit.\n");
+	printf("1 + 2 = %d/n",  add(1, 2));
      return 0:
 }
